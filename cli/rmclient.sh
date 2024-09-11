eb5cce2b51442f06cee65f1ff1d057536e92233d
David Herron <david@davidherron.com>
2024-09-11 13:56:43 +0300

    Initial revision

---
+++ cli/rmclient.sh
@@ -0,0 +1,5 @@
+
+# Call ORY to delete a client by client_id
+ory delete oauth2-client $1
+
+
---
+++ hydra/rmclient.sh
@@ -0,0 +1,5 @@
+
+# Call Hydra to delete a client by client_id
+docker exec -it hydra-hydra-1 hydra delete oauth2-client $1
+
+
